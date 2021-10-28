925679e578d52721ad80aea3b8cd01c3ebc524e7
aksmf1442 <aksmf1442@naver.com>
2021-10-28 12:33:02 +0900

    programmers 문자열 내 마음대로 정렬하기

--- 1.py
+++ 1.py
@@ -1,4 +1,10 @@
-a = list(input().split())
-a.sort()
-print(' '.join(a))
+def cel2fah(celsius):
+    fahrenheit = (9/5) * celsius + 32
+    return fahrenheit;
+
+celsius = 0;
+
+for i in range(celsius, 51, 10):
+    print(i)
+    print(cel2fah(i))
 
